d6edf44b4b1a36254c7a16f99024236cb07a489e
SaeedSatari <saeedsatari93@gmail.com>
2021-01-14 18:30:43 +0330

    -initial commit

---
+++ src/main/java/ir/sattari/security/SecurityExampleApplication.java
@@ -0,0 +1,13 @@
+package ir.sattari.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityExampleApplication.class, args);
+	}
+
+}
